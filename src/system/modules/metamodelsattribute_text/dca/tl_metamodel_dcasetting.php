7a61a6b558ebaad311cad90a051dd331ff596642
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-01-10 15:44:56 +0100

    new dca settings, hope they make sense.

---
+++ src/system/modules/metamodelsattribute_text/dca/tl_metamodel_dcasetting.php
@@ -0,0 +1,26 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['text'] =
+array
+(
+	'backend' => array(
+		'tl_class',
+		'mandatory',
+		'filterable',
+		'sortable',
+		'flag',
+		'searchable',
+	),
+	'advanced' => array
+	(
+		'allowHtml',
+		'preserveTags',
+		'decodeEntities',
+		'trailingSlash',
+		'spaceToUnderscore',
+		'rows',
+		'cols',
+	),
+);
+
+?>
