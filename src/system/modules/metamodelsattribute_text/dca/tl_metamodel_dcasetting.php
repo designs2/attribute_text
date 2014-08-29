e5b713aedec5174d3fd661b5c07a97dc12ce9edc
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-08-29 11:22:37 +0200

    Reindent dca file.

--- src/system/modules/metamodelsattribute_text/dca/tl_metamodel_dcasetting.php
+++ src/system/modules/metamodelsattribute_text/dca/tl_metamodel_dcasetting.php
@@ -17,21 +17,24 @@
 
 $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['text'] = array
 (
-	'presentation' => array(
-		'tl_class',
-	),
-	'functions'  => array(
-		'mandatory',
-		'allowHtml',
-		'preserveTags',
-		'decodeEntities',
-		'trailingSlash',
-		'spaceToUnderscore',
-	),
-	'overview' => array(
-		'filterable',
-		'searchable',
-		'sortable',
-		'flag'
-	)
+    'presentation' => array
+    (
+        'tl_class',
+    ),
+    'functions'    => array
+    (
+        'mandatory',
+        'allowHtml',
+        'preserveTags',
+        'decodeEntities',
+        'trailingSlash',
+        'spaceToUnderscore',
+    ),
+    'overview'     => array
+    (
+        'filterable',
+        'searchable',
+        'sortable',
+        'flag'
+    )
 );
