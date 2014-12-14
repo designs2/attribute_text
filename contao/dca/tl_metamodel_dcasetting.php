4afd96a2a059534a7f866d799097667a5166940d
cboelter <christopher@boelter.eu>
2014-12-14 16:44:25 +0100

    readded author information

--- contao/dca/tl_metamodel_dcasetting.php
+++ contao/dca/tl_metamodel_dcasetting.php
@@ -10,6 +10,9 @@
  * @package     MetaModels
  * @subpackage  AttributeText
  * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author      Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author      Andreas Isaak <info@andreas-isaak.de>
+ * @author      Christopher Boelter <christopher@boelter.eu>
  * @copyright   The MetaModels team.
  * @license     LGPL.
  * @filesource
